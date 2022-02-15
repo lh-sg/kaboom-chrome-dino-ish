a1d6343e3742417ca647870adb9e4bb4505914f4
hani <daiz19@gmail.com>
2022-02-15 10:59:47 +0800

    responsive

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs"
 
+var Width = document.documentElement.clientWidth;
+var Height = document.documentElement.clientHeight;
+
 kaboom({
+  width: Width,
+  height: Height,
   global: true,
   debug: true,
   background: [0,0,0,],
@@ -22,17 +27,16 @@ scene('game', (record) => {
     body(),
   ])
   
-  // keyPress('space', () => { 
-  //   if (mario.grounded()) {
-  //     mario.jump()
-  //   }
-  // })
-  // mouseClick(() => { 
-  //   if (mario.grounded()) {
-  //     mario.jump()
-  //   }
-  // })
-
+  keyPress('space', () => { 
+    if (mario.grounded()) {
+      mario.jump()
+    }
+  })
+  mouseClick(() => { 
+    if (mario.grounded()) {
+      mario.jump()
+    }
+  })
   onTouchStart(() => { 
     if (mario.grounded()) {
       mario.jump()
